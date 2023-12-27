f7e22ad53817da875b9d1450c93d57a4df6a33ac
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-12-27 16:10:52 -0300

    Methods Get e Set

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,59 +1,56 @@
 package application;
+
 import java.util.Locale;
 import java.util.Scanner;
 import entities.Product;
 
-
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
-		
 
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
 		String name = scanner.nextLine();
-		
+
 		System.out.print("Price: ");
 		double price = scanner.nextDouble();
-		
-		
+
 		scanner.nextLine();
-		
+
 		Product product = new Product(name, price);
-		
-		System.out.println(product.name + "," + product.price + "," + product.quantity);
-		
+
+		System.out.println(product.getName() + "," + product.getPrice() + "," + product.getQuantity());
+
 		System.out.println("Product Data: " + product);
-		
+
 		String userOption;
-		
+
 		do {
 			System.out.println("Type [A] to ADD to stock or [R] to REMOVE to stock");
 			userOption = scanner.nextLine().toLowerCase();
-			
+
 			if (userOption.equals("a")) {
 				System.out.println("Enter the number of products to be added in stock: ");
 				int quantity = scanner.nextInt();
 				scanner.nextLine();
 				product.addProducts(quantity);
 				System.out.println("Product Data: " + product);
-			}else if (userOption.equals("r")) {
+			} else if (userOption.equals("r")) {
 				System.out.println("Enter the number of products to be removed from stock: ");
 				int remove = scanner.nextInt();
 				scanner.nextLine();
 				product.removeProducts(remove);
 				System.out.println("Product Data: " + product);
-			}else {
+			} else {
 				System.out.println("Invalid option. Please type [A] to ADD or [R] to REMOVE.");
+				userOption = "";
 			}
-			
-		} while (product.quantity > 0 || (userOption.equals("a") || userOption.equals("r")));
-		
-			
-		
+
+		} while (product.getQuantity() > 0 || userOption.isEmpty());
+
 		scanner.close();
 
 	}
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,9 +2,9 @@ package entities;
 
 public class Product {
 	
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
 	
 	public Product(String name, double price, int quantity) {
 		this.name = name;
@@ -15,7 +15,31 @@ public class Product {
 	public Product(String name, double price){
 		this.name = name;
 		this.price = price;
-			}
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPrice (double price ) {
+		this.price = price;
+	}
+	
+	public void setQuantity (int quantity) {
+		this.quantity = quantity;
+	}
 	
 	public double totalValueInStock() {
 		return price * quantity;
